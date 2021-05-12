d04e8693f9f04d7dd3ca31937af6853277de6208
adekunleba <tonytunde2012@gmail.com>
2021-05-12 23:29:47 +0100

    fix: add api gateway with krakend adekunleba/healthou_backend#13

--- healthou_signup_service/src/db/dbconnection.ts
+++
@@ -1,8 +0,0 @@
-import { Connection, createConnection } from 'typeorm';
-// import mariadb from './dbconfig'
-import maridbConfig = require('./dbconfig');
-export class DbConnection {
-    async getDefaultConnection(): Promise<Connection> {
-        return await createConnection(maridbConfig);
-    }
-}
--- healthou_signup_service/src/errors/base.error.ts
+++
@@ -1,7 +0,0 @@
-export class HealthouUserError extends Error {
-    constructor(message?: string) {
-        super(message);
-        Object.setPrototypeOf(this, new.target.prototype);
-        this.name = HealthouUserError.name;
-    }
-}
